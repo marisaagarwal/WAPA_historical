57b4a3fd641756b52abb0d8043754235d018356b
Marisa Agarwal <marisaagarwal@gmail.com>
2022-03-24 16:49:43 +1000

    initial commit

---
+++ integrate.R
@@ -0,0 +1,70 @@
+##  Project Name:  Historical Coral Data from WAPA 
+##
+##  Objective:     Assess the historical diversity and community structure of 
+##                 coral from surveys at War in the Pacific National Historic
+##                 Park in Guam, USA.
+##
+##  Approach:      1. Obtain historic data from surveys done within WAPA.
+##                 2. Analyze data to fulfill research objective with focus on:
+##                      - diversity
+##                      - community structure & variation (NMDS?)
+##                 3. If possible, compare to modern surveys
+##
+##  Authors:       Marisa Agarwal
+##
+##  Start Date:    2022-03-24
+
+
+##  Notes:         RUN THIS FILE BEFORE EVERY SESSION 
+
+
+
+## 1. point to working directory
+
+    setwd("research/WAPA_historical")
+
+
+## 2. Set up core functionality
+
+    # clean up
+    rm(list=ls())
+    
+    # call to core packages for data manipulation
+    library(dplyr)
+    library(tidyr)
+    library(magrittr)      
+    library(purrr)
+    library(lubridate)
+    library(stringr)
+    library(forcats)      
+    library(tidyverse)
+    library(see)
+    library(broom)
+    
+    # for importing different formats
+    library(readr)
+    library(readxl)
+    library(data.table)
+    
+    # for easier tidy stats 
+    library(rstatix)
+    library(easystats)
+    library(multcomp)
+    library(vegan)
+    
+    # call to visualisation & output generation
+    library(ggplot2)
+    library(GGally)
+    library(Cairo)
+    library(extrafont)
+    library(RColorBrewer)
+    library(viridis)
+    library(ggdist)
+    library(ggsci)
+    library(plotrix)
+    library(multcompView)
+    library(ggpubr)
+
+
+## 3. Generate data objects
+## if things need to be loaded in before every session, include them here
